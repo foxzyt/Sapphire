6614c9e9daa014683b31d37b1f42c48953ef9b10
Bernardo Alvim <126794098+foxzyt@users.noreply.github.com>
2026-01-03 13:19:54 -0300

    Delete src directory

--- src/sapphire_api.h
+++
@@ -1,8 +0,0 @@
-#ifndef SAPPHIRE_API_H
-#define SAPPHIRE_API_H
-
-#include <string>
-
-bool compile_source_to_bytecode_file(const std::string& source_code, const std::string& output_path);
-
-#endif // SAPPHIRE_API_H
